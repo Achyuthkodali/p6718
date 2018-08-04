a88eae275c92be3bc18337e64fb0a4a3d5af2e21
Achyuth <achyuth.kodali97@gmail.com>
2018-08-04 10:25:25 +0530

    4-8-2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'services', component: ServicesComponent},
+      { path: 'form', component: FormComponent}
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
